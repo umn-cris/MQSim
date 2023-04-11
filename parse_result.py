efd4d9032a18f021c996821178d3d36c2ed3ac0e
Chai <hsieh123@umn.edu>
2023-04-11 16:25:11 -0500

    Add tag field in result/PageMap.json

--- parse_result.py
+++ parse_result.py
@@ -15,76 +15,61 @@ import os
 import re
 import xml.etree.ElementTree as ET
 
-suite_tag = sys.argv[1]
-if len(sys.argv) > 2:
-    desc = sys.argv[2]
+suite_tag = ''
+desc = ''
+
 # define the folder path
 folder_path = "results/"
 suite_dict = {'tag': suite_tag, 'tests': []}
+test_tags= {}
 
-# loop through all XML files in the folder, this only applies to PageMap suite
-for folder in os.listdir(folder_path+suite_tag+"/"):
-    path = folder_path+suite_tag+"/"+folder
-    print (f"##### {path} #####\n")
-    
-    for filename in os.listdir(path):
-        if filename.endswith(".xml"):
-            # parse XML data
-            file_path = os.path.join(path, filename)
-            workload = ET.parse(file_path).find(".//Name").text
-            root = ET.parse(file_path).getroot()
-            bandwidth = float(root.find(".//Bandwidth").text)
-            iops = float(root.find(".//IOPS").text)
-            Device_Response_Time = root.find(".//Device_Response_Time").text
-            # round them to 2 decimal places
-            bandwidth = round(bandwidth, 2)
-            iops = round(iops, 2)
-            
-            interleave_program_cmd = root.find('.//SSDDevice.FTL').attrib['Issued_Flash_Interleaved_Program_CMD']
-            multiplane_program_cmd = root.find('.//SSDDevice.FTL').attrib['Issued_Flash_Multiplane_Program_CMD']
-            copy_program_cmd = root.find('.//SSDDevice.FTL').attrib['Issued_Flash_Copyback_Program_CMD']
-
-            # Define the XPath query
-            xpath_query = './/SSDDevice.TSU.User_Write_TR_Queue.Priority.HIGH'
-
-            # Extract the values of Avg_Queue_Length and STDev_Queue_Length
-            avg_queue_lengths = []
-            stdev_queue_lengths = []
-            
-            for element in root.findall(xpath_query):
-                avg_queue_lengths.append(float(element.get('Avg_Queue_Length')))
-                stdev_queue_lengths.append(float(element.get('STDev_Queue_Length')))
+def parse(suite, tags = {}):
+    # loop through all XML files in the folder, this only applies to PageMap suite
+    for folder in os.listdir(folder_path+suite+"/"):
+        path = folder_path+suite+"/"+folder
+        print (f"##### {path} #####\n")
+        
+        for filename in os.listdir(path):
+            if filename.endswith(".xml"):
+                # parse XML data
+                file_path = os.path.join(path, filename)
+                workload = ET.parse(file_path).find(".//Name").text
+                root = ET.parse(file_path).getroot()
+                bandwidth = float(root.find(".//Bandwidth").text)
+                iops = float(root.find(".//IOPS").text)
+                Device_Response_Time = root.find(".//Device_Response_Time").text
+                # round them to 2 decimal places
+                bandwidth = round(bandwidth, 2)
+                iops = round(iops, 2)
                 
-            # Compute the average of Avg_Queue_Length and STDev_Queue_Length
-            avg_avg_queue_length = round(sum(avg_queue_lengths) / len(avg_queue_lengths),2)
-            avg_stdev_queue_length = round(sum(stdev_queue_lengths) / len(stdev_queue_lengths),2)
+                interleave_program_cmd = root.find('.//SSDDevice.FTL').attrib['Issued_Flash_Interleaved_Program_CMD']
+                multiplane_program_cmd = root.find('.//SSDDevice.FTL').attrib['Issued_Flash_Multiplane_Program_CMD']
+                copy_program_cmd = root.find('.//SSDDevice.FTL').attrib['Issued_Flash_Copyback_Program_CMD']
 
-            # extract experiment tags
-            request_size = re.search(r'(\d+)KB', filename)
-            if re.search(r'sequential', filename):
-                workload_type = 'sequential'
-            elif re.search(r'random', filename):
-                workload_type = 'random'
-            elif re.search(r'mix', filename):
-                workload_type = 'mix'
-            else:
-                workload_type = None
-            if re.search(r'write', filename):
-                workload_type2 = 'write'
-            elif re.search(r'read', filename):
-                workload_type2 = 'read'
-            else:
-                workload_type2 = None
-            write_percent = re.search(r'(\d+)pct', filename)
-            if suite_tag == 'ZoneSize':
-                zone_size = desc
-            else:
-                zone_size = 'default'
+                # Define the XPath query
+                xpath_query = './/SSDDevice.TSU.User_Write_TR_Queue.Priority.HIGH'
 
-            expriment_tag = {'req_size': request_size.group(1), 'workload_type': workload_type, 'workload_type2': workload_type2, 'write_percent': write_percent.group(1), 'zone_size': zone_size}
-            # append the result to the suite_dict
-            suite_dict.get("tests").append({'tags': expriment_tag, 'desc': folder, 'scenario': filename, 'workload': workload, 'bandwidth': bandwidth, 'iops': iops, 'Device_Response_Time': Device_Response_Time, 'interleave_program_cmd': interleave_program_cmd, 'multiplane_program_cmd': multiplane_program_cmd, 'copy_program_cmd': copy_program_cmd, 'Average Avg_Queue_Length': avg_avg_queue_length, 'Average STDev_Queue_Length': avg_stdev_queue_length})
+                # Extract the values of Avg_Queue_Length and STDev_Queue_Length
+                avg_queue_lengths = []
+                stdev_queue_lengths = []
+                
+                for element in root.findall(xpath_query):
+                    avg_queue_lengths.append(float(element.get('Avg_Queue_Length')))
+                    stdev_queue_lengths.append(float(element.get('STDev_Queue_Length')))
+                    
+                # Compute the average of Avg_Queue_Length and STDev_Queue_Length
+                avg_avg_queue_length = round(sum(avg_queue_lengths) / len(avg_queue_lengths),2)
+                avg_stdev_queue_length = round(sum(stdev_queue_lengths) / len(stdev_queue_lengths),2)
 
-with open(folder_path+suite_tag+'.json', 'w') as json_file:
-    json.dump(suite_dict, json_file,  indent = 4)
+                # append the result to the suite_dict
+                suite_dict.get("tests").append({'tags': tags, 'scenario': filename, 'workload': workload, 'bandwidth': bandwidth, 'iops': iops, 'Device_Response_Time': Device_Response_Time, 'interleave_program_cmd': interleave_program_cmd, 'multiplane_program_cmd': multiplane_program_cmd, 'copy_program_cmd': copy_program_cmd, 'Average Avg_Queue_Length': avg_avg_queue_length, 'Average STDev_Queue_Length': avg_stdev_queue_length})
+                jstr = json.dumps(suite_dict, indent = 4)
 
+    with open(folder_path+suite+'.json', 'a') as json_file:
+        json_file.write(jstr + '\n')
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        suite_tag = sys.argv[1]
+        test_tags = sys.argv[2]
+    parse(suite_tag, test_tags)
--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,11 @@
+# Usage: python3 run_tests.py
+# This script will run all tests specified in run_tests.config
+# Suite tag name in run_test.config must match the folder name
+# in workload/ and results/ folder
+
 import os
 import xml.etree.ElementTree as ET
+from parse_result import parse
 
 tree = ET.parse('run_tests.config')
 root = tree.getroot()
@@ -12,40 +18,52 @@ def index_of_nth(text, substring, n):
         index = text.index(substring, index) + 1
     return index - 1
 
+def main():
+    for suite in root.iter('Suite'):
+        tag = suite.attrib["tag"]
+        run = suite.attrib["run"].title() == 'True'
+        if run:
+            for test in suite.iter('Test'):
+                ssdcfg = test.find('SSDConfig').text
+                workload = test.find('Workload').text
+                test_tags = gather_tags(test)
 
-for suite in root.iter('Suite'):
-    tag = suite.attrib["tag"]
-    run = suite.attrib["run"].title() == 'True'
-    if run:
-        for test in suite.iter('Test'):
-            ssdcfg = test.find('SSDConfig').text
-            workload = test.find('Workload').text
-            desc = test.attrib['desc'].title().capitalize()
-            if tag == "PageMap":
-                result_dir = "results/"+tag+"/"+desc
-            else:
-                result_dir = "results/"+tag
+                if tag == "PageMap":
+                    result_dir = "results/"+tag+"/"+test_tags['desc']
+                else:
+                    result_dir = "results/"+tag
 
-            # run individual tests spicified by suite "run" attribute
-            # if run==false, but uid is specified as arg, run it
-            
-            if not os.path.exists(result_dir):
-                os.makedirs(result_dir)
+                # run individual tests spicified by suite "run" attribute
+                # if run==false, but uid is specified as arg, run it
+                
+                if not os.path.exists(result_dir):
+                    os.makedirs(result_dir)
 
-            # result = result_dir+workload[index_of_nth(workload,"/",2 ):-4]+"_scenario_1"+workload[-4:]
-            # print (ssdcfg, workload, result)
+                cmd = os.getcwd()+"/MQSim -i "+ssdcfg+" -w "+workload
+                os.system(cmd)
+        
+                # move all scenario xml to result directory
+                os.chdir('workload/'+tag)
+                os.system('mv *_scenario* ../../'+result_dir)
+                os.chdir(cwd)
 
-            cmd = os.getcwd()+"/MQSim -i "+ssdcfg+" -w "+workload
-            os.system(cmd)
-    
-            # move all scenario xml to result directory
-            os.chdir('workload/'+tag)
-            os.system('mv *_scenario* ../../'+result_dir)
-            os.chdir(cwd)
+                # Create a json file for each suite, `parse_result.py` will parse each Test in the Suite (e.g. CDWP, CDPW ... in PageMap)
+                # and produce a joint json file for each suite, named after the suite tag (e.g. results/PageMap/PageMap.json)
+                # run parse_result.py will create for '' PageMap, and '' for others
+                parse(tag, test_tags)
 
-        # Create a json file for each suite, `parse_result.py` will parse each Test in the Suite (e.g. CDWP, CDPW ... in PageMap)
-        # and produce a joint json file for each suite, named after the suite tag (e.g. results/PageMap/PageMap.json)
-        # run parse_result.py will create for '' PageMap, and '' for others
-        os.system('python3 parse_result.py '+tag+' '+desc)
-        # add workload name to result json file
-        os.rename('results/'+tag+'/'+tag+'.json', 'results/'+tag+'/'+tag+'_'+workload[index_of_nth(workload,"/",2 ):-4]+'.json')
+
+
+def gather_tags(test):
+    """Gather test tags"""
+    tags = {}
+    tags['desc'] = test.attrib['desc'].title().capitalize()
+    tags['request_size'] = test.attrib['request_size'].title().upper()
+    tags['workload_type'] = test.attrib['workload_type'].title().lower()
+    tags['workload_type2'] = test.attrib['workload_type2'].title().lower()
+    tags['write_percent'] = test.attrib['write_percent'].title()
+    tags['zone_size'] = test.attrib['zone_size'].title()
+    return tags
+
+if __name__ == "__main__":
+    main()
