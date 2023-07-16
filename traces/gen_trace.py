1f4e3853980bbeb7fb1e01203298f393f7d43db2
Chai <hsieh123@umn.edu>
2023-07-16 13:27:09 -0500

    Merged auto gen trace script. trace_maker.cpp produces seq mixed request traces.

--- traces/gen_trace.py
+++ traces/gen_trace.py
@@ -1,29 +1,29 @@
 import os
 
-#req_size_in_kb = ["4", "16", "64", "256", "1024", "4096", "16384", "65536"]
-req_size_in_kb = ["4","8","16","32","64"]
-intensity_us = ["60"]
+req_size_in_kb = ["4", "8", "16", "32", "64"]
+intensity_us = ["50","52","54","56","58","60"]
 trace_size_in_GB = "1"
-begin_zone = [1,17,33,49]
-#begin_zone = [16*i+1 for i in xrange(32)]
-type_of_request = "write"
-
+begin_zone = [0,1,17,33,49]
+type_of_request = ["write", "read", "mixed"]
+read_percentage = [10, 25, 50, 75, 90]
 os.chdir('trace_files')
+count = 0
 
 for req_size in req_size_in_kb:
     for zone in begin_zone:
         for intensity in intensity_us:
-            trace_file = "sequential_"+type_of_request+"_" + req_size+ "KB_from_zone"+str(zone)+ "_" + trace_size_in_GB + "GB_"+intensity+"us"
-            #print (trace_file)
-
-            file_exists = os.path.exists(trace_file)
-            #print (file_exists)
-            
-            if not file_exists:
-                if type_of_request == "read":
-                    type_of_request = "1"
-                else:
-                    type_of_request = "0"
-                cmd = os.getcwd()+"/trace_generator "+req_size+" "+str(zone)+" "+trace_size_in_GB+" "+type_of_request+" "+intensity
-                os.system(cmd)
-                print ("Created "+trace_file)
+            for tor, type in enumerate(type_of_request):
+                for read_percent in read_percentage:
+                    if type == "mixed":
+                        trace_file_seq = "sequential_"+type+"_read"+str(read_percentage)+"_" + req_size+ "KB_from_zone"+str(zone)+ "_" + trace_size_in_GB + "GB_"+intensity+"us"
+                        trace_file_rand = "random_"+type+"_read"+str(read_percentage)+"_" + req_size+ "KB_from_zone"+str(zone)+ "_" + trace_size_in_GB + "GB_"+intensity+"us"
+                    else:
+                        trace_file_seq = "sequential_"+type+"_" + req_size+ "KB_from_zone"+str(zone)+ "_" + trace_size_in_GB + "GB_"+intensity+"us"
+                        trace_file_rand = "random_"+type+"_" + req_size+ "KB_from_zone"+str(zone)+ "_" + trace_size_in_GB + "GB_"+intensity+"us"
+                
+                    if not os.path.exists(trace_file_seq):
+                        cmd = os.getcwd()+"/trace_generator "+req_size+" "+str(zone)+" "+trace_size_in_GB+" "+str(tor)+" "+str(read_percent)+" "+intensity
+                        os.system(cmd)
+                    if not os.path.exists(trace_file_rand):
+                        cmd = os.getcwd()+"/trace_generator_random "+req_size+" "+str(zone)+" "+trace_size_in_GB+" "+str(tor)+" "+str(read_percent)+" "+intensity
+                        os.system(cmd)
--- traces/trace_maker.cpp
+++ traces/trace_maker.cpp
@@ -13,7 +13,8 @@ unsigned int smallest_zone_number = 2;
 unsigned int biggest_zone_number = 14;
 unsigned int total_size_of_accessed_file_in_GB = 1;
 unsigned int total_no_of_request = 10000;
-unsigned int read_write = 1;
+unsigned int read_write = 1; // 0: write, 1: read, 2: mixed read/write
+unsigned int read_percentage = 50; // used only when read_write == 2
 unsigned int intensity_us = 60;
 string arrival_time;
 string start_LBA;
@@ -23,9 +24,9 @@ string type_of_request;
 
 int main(int argc, char** argv) {
 
-    if (argc != 6) {
+    if (argc != 7) {
         cout << "This generates a sequential write requets." << endl;
-        cout << "Usage: ./trace_generator [request size in KB] [begin zone number] [total request size] [read(1)/write(0)] [intensity in us]"<<endl;
+        cout << "Usage: ./trace_generator [request size in KB] [begin zone number] [total request size] [write(0)/read(1)/mixed(2)] [read_percentage] [intensity in us]"<<endl;
         return -1;
     }
 
@@ -33,13 +34,25 @@ int main(int argc, char** argv) {
     smallest_zone_number = atoi(argv[2]);
     total_size_of_accessed_file_in_GB = atoi(argv[3]);
     read_write = atoi(argv[4]);
-    intensity_us = atoi(argv[5]);
+    read_percentage = atoi(argv[5]);
+    intensity_us = atoi(argv[6]);
 
-    type_of_request = read_write==1?"read":"write";   // 0 means that it's a write reqeust, read request is "1"
+    switch(read_write) {
+        case 0: type_of_request = "write";
+            break;
+        case 1: type_of_request = "read";
+            break;
+        case 2: type_of_request = "mixed_read"+to_string(read_percentage);
+            break;
+        default:
+            cout << "read_write should be 0, 1, or 2" << endl;
+            return -1;
+    }
+    
 
     string outputfile = "sequential_"+type_of_request+"_" + to_string(request_size_in_KB)+ "KB_from_zone"  \
                         + to_string(smallest_zone_number) + "_" + to_string(total_size_of_accessed_file_in_GB) + "GB_" + to_string(intensity_us) + "us";
-    cout << "output file name is " << outputfile << endl;
+    cout << "output file: " << outputfile << endl;
 
     srand(time(NULL));
 
@@ -49,7 +62,6 @@ int main(int argc, char** argv) {
 
     //cout << "request size is " << request_size_in_KB << " KB" << endl;
     total_no_of_request = total_size_of_accessed_file_in_GB * 1024 * 1024 / request_size_in_KB;
-    cout << "total number of requests is " << total_no_of_request << endl;
 
     // unsigned long long int first_arrival_time = 4851300;
     unsigned long long int first_arrival_time = 51300;
@@ -60,12 +72,22 @@ int main(int argc, char** argv) {
 
     unsigned long long int prev_arrival_time = first_arrival_time;
     unsigned long long int prev_start_LBA = first_start_LBA;
+    unsigned int read_count = 0, write_count = 0;
     for (int i = 0; i < total_no_of_request; i++) {
         arrival_time = to_string(prev_arrival_time);
         start_LBA = to_string(prev_start_LBA);
         device_number = "1";
         request_size_in_sector = to_string(request_size_in_KB * 2); // 2 == 1024 / sector_size_in_bytes
         
+        if (type_of_request == "mixed_read"+to_string(read_percentage)) {
+            if (rand() % 100 < read_percentage) {
+                read_write = 1;
+                read_count++;
+            } else {
+                read_write = 0;
+                write_count++;
+            }
+        }
         trace_line = arrival_time + " " + device_number + " " + start_LBA + " " + request_size_in_sector + " " + to_string(read_write) + "\n";
         
         writeFile.write(trace_line.c_str(), trace_line.size());
@@ -77,6 +99,7 @@ int main(int argc, char** argv) {
         prev_arrival_time = prev_arrival_time + (intensity_us * 1000);
         prev_start_LBA = prev_start_LBA + request_size_in_KB * 2; // 2 == 1024 / sector_size_in_bytes;
     }
+    cout << "total number of requests: " << total_no_of_request << ", read count: " << read_count << ", write count: "<< write_count << endl;
     writeFile.close();
     return 0;
 }
